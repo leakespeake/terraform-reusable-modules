86623b896b965b42e251c360c4d6461c4a8e2e86
leakespeake <barryleake@hotmail.com>
2024-02-16 13:11:03 +0000

    provider version updates

--- docker-containers/cadvisor/versions.tf
+++ docker-containers/cadvisor/versions.tf
@@ -3,8 +3,8 @@ terraform {
     # Recommended to pin the specific version of the Docker Provider as new versions are released often
     docker = {
       source  = "kreuzwerker/docker"
-      version = "2.24.0"
+      version = "3.0.2"
     }
   }
-  required_version = ">= 1.3.4"
+  required_version = ">= 1.7.0"
 }
